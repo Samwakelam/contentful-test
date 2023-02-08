5aadc586e8d428f9bfb15af86b92e028fa59497d
Samwakelam <sam.wakelam@gymshark.com>
2023-02-08 11:33:42 +0000

    feat: setting up packages

---
+++ packages/library/vite.config.ts
@@ -0,0 +1,32 @@
+/// <reference types="vitest" />
+
+import { ConfigEnv, defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import * as process from 'process';
+
+// https://vitejs.dev/config/
+
+export default defineConfig((_: ConfigEnv) => {
+  const isDevelop = ['development'].includes(
+    process.env.NODE_ENV ?? 'development'
+  );
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [
+        {
+          find: /(.*).service/,
+          replacement: isDevelop ? '$1.mock' : '$1.service',
+        },
+      ],
+    },
+    // @ts-ignore
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      includeSource: ['lib/**/*.{ts}'],
+    },
+  };
+});
