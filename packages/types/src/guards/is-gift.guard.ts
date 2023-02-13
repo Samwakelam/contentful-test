0eeb3339cbf0a8789ab202190022b78572f28da3
Samwakelam <sam.wakelam@gymshark.com>
2023-02-13 12:01:00 +0000

    feat: remove irrelevant guards

--- packages/types/src/guards/is-gift.guard.ts
+++
@@ -1,7 +0,0 @@
-import { Gift } from '../models';
-
-export const isGift = (value: any): value is Gift => {
-  return (
-    typeof value.properties !== 'undefined' && typeof value._id !== 'undefined'
-  );
-};
--- packages/types/src/guards/is-list.guard.ts
+++
@@ -1,5 +0,0 @@
-import { List } from '../models';
-
-export const isList = (value: any): value is List => {
-  return typeof value.gifts !== 'undefined';
-};
